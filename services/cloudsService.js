8ac6ca9caebd278f698d4d03b56a7e00cfa14d19
Anton <60780638+antonio63963@users.noreply.github.com>
2023-03-06 20:05:30 +0200

    added function setDeletaValue to change whole charts size on very big and middle prices

--- services/cloudsService.js
+++ services/cloudsService.js
@@ -1,35 +1,48 @@
-function getBestPrice(...cloudsArr) {
+function getBestPrice(chartConfig, ...cloudsArr) {
   cloudsArr.forEach((cloud) => {
     cloud.isPriceTheBest = false;
-    cloud.calculateTotalPrice(storageValue, transferValue);
+    cloud.calculateTotalPrice(
+      chartConfig.storageValue,
+      chartConfig.transferValue
+    );
   });
   cloudsArr.sort((a, b) =>
     a.totalPrice < b.totalPrice ? -1 : b.totalPrice < a.totalPrice ? 1 : 0
   );
-  if (storageValue > 0 || transferValue > 0) {
+  if (chartConfig.storageValue > 0 || chartConfig.transferValue > 0) {
     cloudsArr[0].isPriceTheBest = true;
+    const theWorstPrice = cloudsArr[cloudsArr.length - 1].totalPrice;
+    setDeltaValue(theWorstPrice, chartConfig);
   }
-}
+};
+
+function setDeltaValue(theWorstPrice, chartConfig) {
+  if (theWorstPrice > 45) {
+    chartConfig.delta = 3;
+  } else if (theWorstPrice > 20) {
+    chartConfig.delta = 5;
+  } else if (theWorstPrice <= 20) {
+    chartConfig.delta = 10;
+  };
+};
 
 function setChartStyle(chat, price, delta, color) {
   chat.style.cssText = `
-    ${window.innerWidth <= 715 ? "height" : "width"}: ${
-    price * (price > 45 ? delta -6 : delta)
-  }px;
+    ${window.innerWidth <= 715 ? "height" : "width"}: ${price * delta}px;
     background-color: ${color};
   `;
-}
+};
 
 function totalPriceContent(chatPrice, price) {
   chatPrice.textContent = `${price.toFixed(2)}$`;
-}
+};
 
-function onCloudChange(chartElem, textPriceElem, cloudData, delta) {
+function onCloudChange(chartElem, textPriceElem, cloudData, chartConfig) {
   setChartStyle(
     chartElem,
     cloudData.totalPrice,
-    delta,
-    cloudData.usageColor(baseColor)
+    chartConfig.delta,
+    cloudData.usageColor(chartConfig.baseColor)
   );
   totalPriceContent(textPriceElem, cloudData.totalPrice);
-}
+};
